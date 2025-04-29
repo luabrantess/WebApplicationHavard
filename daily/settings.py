6ee917944eb4d82d29262013c15a4190d92d5469
luabrantess <analuizaabr4@gail.com>
2025-04-29 17:26:22 -0300

    Configurando Host

--- daily/settings.py
+++ daily/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@ski1u$r8#dv3%=a9v@jo5lgbu(@*fgsf_3#skqgd0w@*vxoa4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['havard-project.onrender.com']
 
 
 # Application definition
