9af615c219d74b1bbd017ae1f267314223b99900
luabrantess <analuizaabr4@gail.com>
2025-05-04 16:01:25 -0300

    using variable

--- tasks/views.py
+++ tasks/views.py
@@ -3,14 +3,15 @@ import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+tasks = ["foo", "bar", "baz"]
+
 def index(request, name):
     now = datetime.datetime.now()
     return render(request, "tasks/index.html", {
+        "tasks": tasks,
         "name": name.capitalize(),
         "day": now.day,
         "month": now.strftime("%B"),
         "year": now.year
     })
 
-def greet(request, name):
-    return HttpResponse(f"Ola, {name.capitalize()}")
