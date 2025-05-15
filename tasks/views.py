e8bd77799981389ab93cbfb9dd137f3d2d650754
luabrantess <analuizaabr4@gail.com>
2025-05-15 14:29:40 -0300

    creating form in view

--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,27 @@
 import datetime
 
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
 
+class NewTask(forms.Form):
+    task = forms.CharField(label="New task")
+
 def index(request, name):
     now = datetime.datetime.now()
+
+    if request.method == "POST":
+        form = NewTask(request.POST)
     return render(request, "tasks/index.html", {
-        "tasks": tasks,
         "name": name.capitalize(),
         "day": now.day,
         "month": now.strftime("%B"),
         "year": now.year
+    },
+    {
+        "form": NewTask()
     })
+
 
